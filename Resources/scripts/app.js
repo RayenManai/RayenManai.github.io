dbcedf5679c815ae7405d6c4c6b5bf9cf2f9cec9
RayenManai <rayenmannai77@gmail.com>
2024-04-06 15:43:21 +0200

    update layout

--- Resources/scripts/app.js
+++ Resources/scripts/app.js
@@ -13,3 +13,47 @@ document.addEventListener("DOMContentLoaded", function() {
     var yearRange = startYear + " - " + currentYear;
     document.getElementById("copyright").innerHTML = "&copy;" + yearRange + " Rayen Manai";
 });
+
+function leftScroll() {
+    const scrollImages = document.querySelector(".layout");
+    scrollImages.scrollBy({
+        left: -200,
+        behavior: "smooth"
+    });
+}
+
+function rightScroll() {
+    const scrollImages = document.querySelector(".layout");
+    scrollImages.scrollBy({
+        left: 200,
+        behavior: "smooth"
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollImages = document.querySelector(".layout");
+    const scrollLength = scrollImages.scrollWidth - scrollImages.clientWidth;
+    const leftButton = document.querySelector(".left");
+    const rightButton = document.querySelector(".right");
+
+    function checkScroll() {
+        const currentScroll = scrollImages.scrollLeft;
+        if (currentScroll === 0) {
+            leftButton.setAttribute("disabled", "true");
+            rightButton.removeAttribute("disabled");
+        } else if (currentScroll === scrollLength) {
+            rightButton.setAttribute("disabled", "true");
+            leftButton.removeAttribute("disabled");
+        } else {
+            leftButton.removeAttribute("disabled");
+            rightButton.removeAttribute("disabled");
+        }
+    }
+
+    scrollImages.addEventListener("scroll", checkScroll);
+    window.addEventListener("resize", checkScroll);
+    checkScroll();
+
+    leftButton.addEventListener("click", leftScroll); 
+    rightButton.addEventListener("click", rightScroll); 
+});

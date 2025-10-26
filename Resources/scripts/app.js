c74e7b554580dee5aa740dcbb0a6d4474428bc47
RayenManai <rayenmannai77@gmail.com>
2025-10-26 20:49:54 +0100

    update

--- Resources/scripts/app.js
+++ Resources/scripts/app.js
@@ -15,54 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
     "&copy;" + yearRange + " Rayen Manai";
 });
 
-let currentCardIndex = 0;
-let currentMasterCardIndex = 0;
-const cards = document.querySelectorAll(".studium_card");
-const masterCards = document.querySelectorAll(".studium_card_master");
-
-function showCard(index) {
-  cards.forEach((card, i) => {
-    if (i === index) {
-      card.classList.add("active");
-    } else {
-      card.classList.remove("active");
-    }
-  });
-}
-
-function showMasterCard(index) {
-  masterCards.forEach((card, i) => {
-    if (i === index) {
-      card.classList.add("active");
-    } else {
-      card.classList.remove("active");
-    }
-  });
-}
-
-function showNextCard() {
-  currentCardIndex = (currentCardIndex + 1) % cards.length;
-  showCard(currentCardIndex);
-}
-
-function showPrevCard() {
-  currentCardIndex = (currentCardIndex - 1 + cards.length) % cards.length;
-  showCard(currentCardIndex);
-}
-
-function showNextMasterCard() {
-  currentCardIndex = (currentCardIndex + 1) % masterCards.length;
-  showMasterCard(currentCardIndex);
-}
-
-function showPrevMasterCard() {
-  currentCardIndex = (currentCardIndex - 1 + cards.length) % masterCards.length;
-  showMasterCard(currentCardIndex);
-}
-
-// Initially show the first card
-showCard(currentCardIndex);
-
 let currentSlide = 1;
 showSlide(currentSlide);
 
